edf77c954014f1d80656006ce415abbc0241742a
Ritul <rituljain.nitt@gmail.com>
2015-06-24 21:31:09 +0530

    Connection is to be made with the main HTML file.

--- api_connect.php
+++ api_connect.php
@@ -1,13 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<style>
+body{
+	font-size:20px;
+}
+</style>
+</head>
+<body>
 <?php
+//designing part remaining.
 
-echo "hello";
+echo "<div style ='font:23px Arial,tahoma,sans-serif;color:brown'>"."Repo-List ="."</div>";
+echo nl2br("\n");
+//$username=$_POST['username1'];
 $username="Lutir";
 /* gets url */
 function get_content_from_github($url) {
 	$ch = curl_init();
 	curl_setopt($ch,CURLOPT_URL,$url);
 	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1); 
-	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,1);
+	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,100);
 	curl_setopt($ch,CURLOPT_USERAGENT,'Hello Git!');
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	$content = curl_exec($ch);
@@ -23,16 +36,15 @@ $left="https://api.github.com/users/";
 $right="/repos";
 $url=$left.$username.$right;
 $abc = get_content_from_github($url);
+$out=json_decode($abc,true);
+
+
 $i=0;
-for($i=0;$i<strlen($abc);$i++){
-	if($abc[$i]==','){
-		echo nl2br(".\n");	  
-		
-	}
-	echo $abc[$i];	
+for($i=0;$i<100;$i++){
+	echo nl2br("\n");
+echo @"<div style ='font:19px Arial,tahoma,sans-serif;color:blue'>".@$out[$i]['name']."</div>";
+
 }
-
-
-
-
 ?>
+</body>
+</html>
